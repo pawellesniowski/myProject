e23de7f119e7958ec1e8fb17c55e0bc0985f46cb
pawel <pawel.html@gmail.com>
2017-07-01 22:25:53 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const hbs = require('hbs');
+var port = process.env.PORT || 3002;
+var year = new Date().getFullYear();
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+hbs.registerHelper('getFullYear', ()=>{
+    return new Date().getFullYear();
+});
+
+app.set('view engine', 'hbs');
+
+
+
+app.get('/', (req, res)=>{
+    res.render('index.hbs', {
+        pageTitle: "Portfolio",
+    });
+});
+
+app.get('/projects', (req, res)=>{
+    res.render('projects.hbs', {
+        pageTitle: "Portfolio",
+    });
+})
+
+app.listen(port, ()=>{
+    console.log('listen on port 3002');
+});
